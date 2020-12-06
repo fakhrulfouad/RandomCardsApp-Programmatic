7cb9317658458e452a32c20cca55027bc2512a38
Muhammad Fakhrulghazi bin Mohd Fouad <muhammadfakhrulghazibinmohdfouad@Muhammads-MacBook-Pro.local>
2020-12-06 22:45:07 +0800

    testing

--- Programmatic RandomCards/CardSelectionVC.swift
+++ Programmatic RandomCards/CardSelectionVC.swift
@@ -9,21 +9,96 @@ import UIKit
 
 class CardSelectionVC: UIViewController {
 
+    let cardImageView = UIImageView()
+    let stopButton = PRCButton(backgroundColour: .systemRed, title: "STOP")
+    let resetButton = PRCButton(backgroundColour: .systemGreen, title: "RESET")
+    let rulesButton = PRCButton(backgroundColour: .systemBlue, title: "RULES")
+    
+    var cards: [UIImage] = Deck.allValues
+    var timer: Timer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        configureUI()
+        startTimer()
+    }
 
-        // Do any additional setup after loading the view.
+    func startTimer() {
+        timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(showRandomCard), userInfo: nil, repeats: true)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @objc func stopTimer() {
+        timer.invalidate()
     }
-    */
+    
+    @objc func resetTimer() {
+        stopTimer()
+        startTimer()
+    }
+    
+    @objc func showRandomCard() {
+        cardImageView.image = cards.randomElement()
+    }
+    
+    @objc func presentRulesVC() {
+        present(RulesVC(), animated: true)
+    }
+    
+    func configureUI() {
+        configureCardImageView()
+        configureStopButton()
+        configureResetButton()
+        configureRulesButton()
+    }
+    
+    func configureCardImageView() {
+        view.addSubview(cardImageView)
+        cardImageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            cardImageView.widthAnchor.constraint(equalToConstant: 250),
+            cardImageView.heightAnchor.constraint(equalToConstant: 350),
+            cardImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            cardImageView.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -75)
+        ])
+    }
+    
+    func configureStopButton() {
+        view.addSubview(stopButton)
+        stopButton.addTarget(self, action: #selector(stopTimer), for: .touchUpInside)
+        
+        NSLayoutConstraint.activate([
+            stopButton.widthAnchor.constraint(equalToConstant: 260),
+            stopButton.heightAnchor.constraint(equalToConstant: 50),
+            stopButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            stopButton.topAnchor.constraint(equalTo: cardImageView.bottomAnchor, constant: 30)
+        ])
+    }
 
+    func configureResetButton() {
+        view.addSubview(resetButton)
+        resetButton.addTarget(self, action: #selector(resetTimer), for: .touchUpInside)
+        
+        NSLayoutConstraint.activate([
+            resetButton.widthAnchor.constraint(equalToConstant: 115),
+            resetButton.heightAnchor.constraint(equalToConstant: 50),
+            resetButton.leadingAnchor.constraint(equalTo: stopButton.leadingAnchor),
+            resetButton.topAnchor.constraint(equalTo: stopButton.bottomAnchor, constant: 20)
+        ])
+    }
+        
+    func configureRulesButton() {
+        view.addSubview(rulesButton)
+        //add action on button when target
+        rulesButton.addTarget(self, action: #selector(presentRulesVC), for: .touchUpInside)
+        
+        NSLayoutConstraint.activate([
+            rulesButton.widthAnchor.constraint(equalToConstant: 115),
+            rulesButton.heightAnchor.constraint(equalToConstant: 50),
+            rulesButton.trailingAnchor.constraint(equalTo: stopButton.trailingAnchor),
+            rulesButton.topAnchor.constraint(equalTo: stopButton.bottomAnchor, constant: 20)
+            ])
+        }
+ 
 }
--- Programmatic RandomCards/PRCButton.swift
+++ Programmatic RandomCards/PRCButton.swift
@@ -8,13 +8,33 @@
 import UIKit
 
 class PRCButton: UIButton {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
     }
-    */
+    
+    //if u wanna initialise this button using storyboard. apple gives this
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    //creates a custom initiliser that takes in a background color and title as parameter when we initialise pcrbutton so that anytime we crate pcrbutton we it give title and background color
+    init(backgroundColour: UIColor, title: String) {
+        //anytime we create init we need to call super
+        //frame can be zero zero because we using constrainst later.
+        super.init(frame: .zero)
+        self.backgroundColor = backgroundColour
+        setTitle(title, for: .normal)
+        configure()
+    }
+    
+    //configure button
+    func configure() {
+        layer.cornerRadius = 8
+        titleLabel?.font = .systemFont(ofSize: 19, weight: .bold)
+        setTitleColor(.white, for: .normal)
+        translatesAutoresizingMaskIntoConstraints = false
+    }
 
 }
