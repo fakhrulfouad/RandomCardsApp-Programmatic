94ef4c4cfd9c0f84f171406f569cb464dcb6595e
Muhammad Fakhrulghazi bin Mohd Fouad <muhammadfakhrulghazibinmohdfouad@Muhammads-MacBook-Pro.local>
2020-12-06 22:43:58 +0800

    first commit

---
+++ Programmatic RandomCards/Deck.swift
@@ -0,0 +1,8 @@
+//
+//  Deck.swift
+//  Programmatic RandomCards
+//
+//  Created by Muhammad Fakhrulghazi bin Mohd Fouad on 06/12/2020.
+//
+
+import Foundation
---
+++ Programmatic RandomCards/PRCButton.swift
@@ -0,0 +1,20 @@
+//
+//  PRCButton.swift
+//  Programmatic RandomCards
+//
+//  Created by Muhammad Fakhrulghazi bin Mohd Fouad on 05/12/2020.
+//
+
+import UIKit
+
+class PRCButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
